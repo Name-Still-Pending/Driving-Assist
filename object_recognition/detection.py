5d197fc8a4edf36e67c26e2da9c5bc6f40038fd5
Poti <podpecan.andraz28@gmail.com>
2023-05-22 10:51:19 +0200

    Detection class, detection results sent over JSON

--- object_recognition/detection.py
+++ object_recognition/detection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+
 import threading
 import redis
 import kafka
@@ -10,92 +11,97 @@ import torch
 import encoding.JSON as je
 from prometheus_client import start_http_server, Counter
 
-counter_neuralnetwork = Counter('nn_detections', 'Number of NN detections')
 
 
-def thread_do_work():
-    # Redis
-    red = redis.Redis()
+class Detection:
+    def __init__(self):
+        self.counter_neuralnetwork = Counter('nn_detections', 'Number of NN detections')
+        self.event = threading.Event()
+        self.thread = threading.Thread(target=lambda: self.thread_do_work())
 
-    # Kafka
-    con_topic = 'frame_notification'
-    prod_topic = 'frame_detection'
-    producer = kafka.KafkaProducer(bootstrap_servers='localhost:9092')
+    def thread_do_work(self):
+        # Redis
+        red = redis.Redis()
 
-    # auto_offset_reset indicates where the consumer starts consuming the message in case of an interruption
-    consumer = kafka.KafkaConsumer(con_topic, bootstrap_servers='localhost:9092', auto_offset_reset='earliest',
-                                   group_id='grp_detection', consumer_timeout_ms=2000)
-    consumer.poll(10000)
-    # PyTorch
-    model = torch.hub.load("ultralytics/yolov5", "yolov5n", trust_repo=True)
-    consumer.topics()
-    while True:
+        # Kafka
+        con_topic = 'frame_notification'
+        prod_topic = 'frame_detection'
+        producer = kafka.KafkaProducer(bootstrap_servers='localhost:9092')
 
-        # Read from Redis when message is received over Kafka
-        consumer.seek_to_end()
-        for raw_message in consumer:
-            msgJSON = je.decode_bin(raw_message.value)
-            message = msgJSON["id"]
+        # auto_offset_reset indicates where the consumer starts consuming the message in case of an interruption
+        consumer = kafka.KafkaConsumer(con_topic, bootstrap_servers='localhost:9092', auto_offset_reset='earliest',
+                                       group_id='grp_detection', consumer_timeout_ms=2000)
+        consumer.poll()
+        # PyTorch
+        # model = torch.hub.load("ultralytics/yolov5", 'custom', path='results/train/exp2/weights/bes.pt', trust_repo=True)
+        model = torch.hub.load("../yolov5", 'custom', source='local', path='results/train/exp2/weights/best.pt')
+        consumer.topics()
+        while True:
 
-            if message == "new_frame":
-                frame_time = datetime.fromtimestamp(raw_message.timestamp / 1000)
-                curr_time = datetime.now()
-                diff = (curr_time - frame_time).total_seconds()
+            # Read from Redis when message is received over Kafka
+            consumer.seek_to_end()
+            for raw_message in consumer:
+                msgJSON = je.decode_bin(raw_message.value)
+                message = msgJSON["id"]
 
-                # Exclude old frames
-                if diff < 2:
-                    frame_temp = np.frombuffer(red.get("frame:latest"), dtype=np.uint8)
+                if message == "new_frame":
+                    frame_time = datetime.fromtimestamp(raw_message.timestamp / 1000)
+                    curr_time = datetime.now()
+                    diff = (curr_time - frame_time).total_seconds()
 
-                    # Convert image
-                    if np.shape(frame_temp)[0] == 1229760:
-                        frame = frame_temp.reshape((480, 854, 3))
+                    # Exclude old frames
+                    if diff < 2:
+                        frame_temp = np.frombuffer(red.get("frame:latest"), dtype=np.uint8)
 
-                        # Detection
-                        results = model(frame)
-                        names = results.names
-                        preds = results.xyxy[0].numpy()
-                        preds_list = []
-                        for pred in preds:
-                            if names[int(pred[-1])] == "person":
-                                preds_list.append(",".join(str(v) for v in pred[0:4]))
+                        # Convert image
+                        if True:
+                            frame = frame_temp.reshape((540, 960, 3))
 
-                        # Send detection data over Kafka
-                        # TODO: Not the best way of sending and reading data with Kafka
-                        #       Modify sending and receiving using value_serializer and value_deserializer and work with JSON on both sides
-                        future = producer.send(prod_topic, str.encode("|".join(preds_list)),
-                                               timestamp_ms=round(time.time() * 1000))
+                            # Detection
+                            results = model(frame)
+                            names = results.names
+                            preds = results.xyxy[0].numpy()
 
-                        # Wait until message is delivered to Kafka
-                        try:
-                            rm = future.get(timeout=10)
-                        except kafka.KafkaError:
-                            pass
+                            detection_message = {
+                                "frame": msgJSON["frame_n"],
+                                "classes": {
+                                    n: np.where(preds[5] == n, preds).tolist() for n in names.keys
+                                }
+                            }
 
-                    counter_neuralnetwork.inc(1)
+                            # Send detection data over Kafka
+                            future = producer.send(prod_topic, je.encode_bin(detection_message),
+                                                   timestamp_ms=round(time.time() * 1000))
 
-        if event.is_set():
-            break
+                            # Wait until message is delivered to Kafka
+                            try:
+                                rm = future.get(timeout=10)
+                            except kafka.KafkaError:
+                                pass
 
-        # Stop loop
-        if event.is_set():
-            break
+                        self.counter_neuralnetwork.inc(1)
 
+                if self.event.is_set():
+                    break
 
-def sigint_handler(signum, frame):
-    event.set()
-    thread.join()
-    exit(0)
+            # Stop loop
+            if self.event.is_set():
+                break
 
+    def sigint_handler(self, signum, frame):
+        self.event.set()
+        self.thread.join()
+        exit(0)
 
-signal.signal(signal.SIGINT, sigint_handler)
+    def run(self):
+        signal.signal(signal.SIGINT, self.sigint_handler)
+        start_http_server(8000)
+        self.thread.start()
+        input("Press CTRL+C or Enter to stop visualization...")
+        self.event.set()
+        self.thread.join()
 
-event = threading.Event()
-thread = threading.Thread(target=lambda: thread_do_work())
 
 if __name__ == "__main__":
-    start_http_server(8000)
-
-    thread.start()
-    input("Press CTRL+C or Enter to stop visualization...")
-    event.set()
-    thread.join()
+    det = Detection()
+    det.run()
