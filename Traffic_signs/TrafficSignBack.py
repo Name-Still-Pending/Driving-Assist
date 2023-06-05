2879a5503817379475180ff39b4ce8e513980f2f
blaz-ermenc <blaz.ermenc@student.um.si>
2023-06-05 15:10:02 +0200

    Update TrafficSignBack.py

--- Traffic_signs/TrafficSignBack.py
+++ Traffic_signs/TrafficSignBack.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import playsound
-import threading
 from queue import Queue
 
 # Create a queue for storing sound files to be played
@@ -14,8 +13,6 @@ def play_sound(sound_file):
 # Load the pre-trained YOLO model and labels
 net = cv2.dnn.readNetFromDarknet('yolo.cfg', 'yolo.weights')
 labels = ['stop', 'yield', 'right_of_way', '30', '40', '50', '60', '70', '80', '100', 'residential_area', 'residential_area_end', 'speed_limit_end', 'end_of_30', 'warning', 'one_way_road', 'bus_stop', 'crosswalk', 'roundebound', 'direction', 'no_entry', 'prohibition', 'ignore', 'info_sign']
-    # List of class labels
-detectedStatus = [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
 
 # Set the paths to the WAV files for each class
 audio_files = {
@@ -43,42 +40,89 @@ audio_files = {
     21: 'Prohibition.wav',
 }
 
-# Load the image
-image = cv2.imread('image.jpg')
+def perform_object_detection(image):
+    # Perform object detection
+    blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(output_layers)
 
-# Perform object detection
-blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
-net.setInput(blob)
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-outputs = net.forward(output_layers)
+    # Process the detections
+    detections = []
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
 
-# Process the detections
-for output in outputs:
-    for detection in output:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
+            if confidence > 0.5:
+                if class_id < 22:
+                    # Add the detection to the list
+                    detections.append((class_id, confidence))
 
-        if confidence > 0.5 :
-            if class_id < 22:
-                # Play the corresponding sound based on the class index
-                if detectedStatus[class_id] == False :
-                    detectedStatus[class_id] = True
-                    sound_file = audio_files[class_id]
-                    sound_queue.put(sound_file)  # Add sound file to the queue
+    return detections
 
-                    if sound_queue.qsize() == 1:  # If it's the only sound in the queue, start playing immediately
-                        sound_thread = threading.Thread(target=play_sound, args=(sound_file,))
-                        sound_thread.start()
+# Function to process frames
+async def process_frames():
+    detectedStatus = [False] * 22
 
-                    if class_id == 0 or class_id == 1:
-                        # send NoPriority
-                        pass
-                    elif class_id == 2:
-                        # send Priority
-                        pass
+    # Create a loop to continuously receive frames
+    while True:
+        currentStatus = [False] * 22
+
+        # Receive a frame from the video loader (replace 'get_frame()' with the appropriate function from your video loader)
+        frame = get_frame()  # TODO!!
+
+        # Perform object detection on the frame
+        detections = perform_object_detection(frame)
+
+        # Process the detections and play sounds
+        for detection in detections:
+            class_id, confidence = detection
+
+            currentStatus[class_id] = True
+
+            if detectedStatus[class_id] == False:
+                detectedStatus[class_id] = True
+                sound_file = audio_files[class_id]
+                sound_queue.put(sound_file)  # Add sound file to the queue
+
+                if sound_queue.qsize() == 1:  # If it's the only sound in the queue, start playing immediately
+                    asyncio.create_task(play_sound())  # Create a task for playing sound asynchronously
+
+                if class_id == 0 or class_id == 1:
+                    # send NoPriority TODO!!
+                    pass
+                elif class_id == 2:
+                    # send Priority TODO!!
+                    pass
 
             # Print the class label and confidence
             label = labels[class_id]
             print(f"Detected: {label} (Confidence: {confidence})")
+
+        detectedStatus = currentStatus.copy()
+        await asyncio.sleep(0)  # Non-blocking delay
+
+# Main code
+if __name__ == "__main__":
+    # Start the event loop
+    loop = asyncio.get_event_loop()
+
+    # Create tasks for playing sounds and processing frames
+    tasks = [
+        asyncio.create_task(play_sound()),
+        asyncio.create_task(process_frames())
+    ]
+
+    # Run the tasks concurrently in the event loop
+    try:
+        loop.run_until_complete(asyncio.gather(*tasks))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Cancel tasks and close the event loop
+        for task in tasks:
+            task.cancel()
+        loop.close()
